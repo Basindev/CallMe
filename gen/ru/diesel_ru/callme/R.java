17da3cd89fe39029dd688b36153dc79cd60870ff
Denis Gamza <diesel_ru@mail.ru>
2013-10-28 15:54:56 +0800

    main interface

--- gen/ru/diesel_ru/callme/R.java
+++ gen/ru/diesel_ru/callme/R.java
@@ -25,15 +25,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int contact_blue=0x7f020000;
+        public static final int contact_white=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int send_blue=0x7f020003;
+        public static final int star_blue=0x7f020004;
+        public static final int star_white=0x7f020005;
+        public static final int start_blue=0x7f020006;
     }
     public static final class id {
         public static final int abaut=0x7f0a0000;
-        public static final int action_settings=0x7f0a0005;
-        public static final int editText1=0x7f0a0002;
-        public static final int editText2=0x7f0a0004;
-        public static final int textView1=0x7f0a0003;
-        public static final int tv=0x7f0a0001;
+        public static final int action_settings=0x7f0a0007;
+        public static final int admobLayout=0x7f0a0006;
+        public static final int btnContacts=0x7f0a0002;
+        public static final int btnFContacts=0x7f0a0003;
+        public static final int btnSend=0x7f0a0005;
+        public static final int etPhone=0x7f0a0004;
+        public static final int textView1=0x7f0a0001;
     }
     public static final class layout {
         public static final int abaut=0x7f030000;
@@ -43,6 +51,7 @@ public final class R {
         public static final int main=0x7f090000;
     }
     public static final class string {
+        public static final int Abonent=0x7f070006;
         public static final int EndUSSD=0x7f070004;
         public static final int PhoneNumber=0x7f070003;
         public static final int StartUSSD=0x7f070005;
--- src/ru/diesel_ru/callme/MainActivity.java
+++ src/ru/diesel_ru/callme/MainActivity.java
@@ -2,20 +2,43 @@ package ru.diesel_ru.callme;
 
 import ru.diesel_ru.callme.Abaut;
 import ru.diesel_ru.callme.PrefActivity;
+import ru.diesel_ru.callme.R;
+import ru.diesel_ru.callme.FavContList;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
 
 public class MainActivity extends Activity {
 
-	//TextView tv;
+	protected static final int PICK_RESULT = 0;
+	static final private int PHONE_NUMBER = 3;
+	
 	String strOprator = "";
 	SharedPreferences sp;
+	
+	ImageButton buttonSelectContact;
+	ImageButton buttonSelectFavoritesContact;
+	ImageButton buttonSend;
+	
+	TextView txtPhoneNumber;
+	
+	AdView adView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +46,12 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		sp = PreferenceManager.getDefaultSharedPreferences(this);
-		strOprator = sp.getString("defaultOperator","1");
+		strOprator = sp.getString("defaultOperator","0");
+        
+		txtPhoneNumber = (TextView) findViewById(R.id.etPhone);
+		buttonSelectContact = (ImageButton) findViewById(R.id.btnContacts);
+        buttonSelectFavoritesContact = (ImageButton) findViewById(R.id.btnFContacts);
+        //buttonSend = (ImageButton) findViewById(R.id.btnSend);
         
         Intent localIntent = getIntent();
         if (localIntent.getAction().contains("android.intent.action.SENDTO")){
@@ -51,26 +79,144 @@ public class MainActivity extends Activity {
         	}else if (strOprator.compareToIgnoreCase("6") == 0) {
         		//"Other":
         		_intent.setData(Uri.fromParts("tel", sp.getString("otherStartUSSD","") + strNum + sp.getString("otherEndUSSD",""), "#"));
+			}else if (strOprator.compareToIgnoreCase("0") == 0) {
+				Toast.makeText(getApplicationContext(), "Выберите оператора связи!", Toast.LENGTH_SHORT).show();
+				return;
 			}
 
 			startActivity(_intent);
 			finish();
         }
         
+      //Создание adView ca-app-pub-9670568035952143/5674883316
+        //adView = new AdView(this, AdSize.BANNER, "a1510fa3b8c4d5e");
+        adView = new AdView(this, AdSize.BANNER, "ca-app-pub-9670568035952143/5674883316");
+        										    
+        // Поиск в LinearLayout (предполагается, что был назначен
+        // атрибут android:id="@+id/mainLayout"
+        LinearLayout layout = (LinearLayout)findViewById(R.id.admobLayout);
+
+        // Добавление adView
+        layout.addView(adView);
+
+        // Инициирование общего запроса на загрузку вместе с объявлением
+        adView.loadAd(new AdRequest());
         
+        
+        // Обработчик выбора контакта
+        buttonSelectContact.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		// Выбор только контактов звонков (без почтовых)
+                Intent pickIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                pickIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+                startActivityForResult(pickIntent, PICK_RESULT);
+        	}
+        });
+        
+        // Обработчик выбора избранного контакта
+        buttonSelectFavoritesContact.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, FavContList.class);
+			    startActivityForResult(intent, PHONE_NUMBER);
+        	}
+        });
+        
+        // Обработчик нажатиЯ на кнопку отправить
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		if (txtPhoneNumber.length() < 1)
+        		{
+        			Toast.makeText(getApplicationContext(), "Введите номер телефона!", Toast.LENGTH_SHORT).show();
+        			return;
+        		}
+        		
+        		sendUSSD(txtPhoneNumber.getText().toString());
+        	}
+        });
 	}
 
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.main, menu);
-        //menu.add("menu1");
-        //return true;
-	      MenuItem mi = menu.add(0, 1, 0, "Ќастройки");
+	      MenuItem mi = menu.add(0, 1, 0, "Настройки");
 	      mi.setIntent(new Intent(this, PrefActivity.class));
-	      mi = menu.add(0, 1, 0, "Ћ программе");
+	      mi = menu.add(0, 1, 0, "О программе");
 	      mi.setIntent(new Intent(this, Abaut.class));
 	      
 	      return super.onCreateOptionsMenu(menu);
     }
 
+    // Обработка выбора контакта
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	// Получаем номер из избранных контактов
+    	if (requestCode == PHONE_NUMBER) {
+    		if (resultCode == RESULT_OK) {
+    			String thiefname = data.getStringExtra(FavContList.PHONE_NUMBER);
+    			txtPhoneNumber.setText(thiefname.replace("+7", "8").replace("-", "").replace(" ", ""));
+    		}else {
+    			txtPhoneNumber.setText(""); // стираем текст
+    		}
+    	}
+    	// Получаем номер из активити контактов
+        if (data != null) {
+            Uri uri = data.getData();
+
+            if (uri != null) {
+                Cursor c = null;
+                try {
+                	c = getContentResolver().query(uri, new String[]{ 
+                            ContactsContract.CommonDataKinds.Phone.NUMBER,
+                            ContactsContract.CommonDataKinds.Phone.TYPE},
+                        null, null, null);
+
+                    if (c != null && c.moveToFirst()) {
+                        String number = c.getString(0);
+                        int type = c.getInt(1);
+                        showSelectedNumber(type, number);
+                    }
+                } finally {
+                    if (c != null) {
+                        c.close();
+                    }
+                }
+            }
+        }
+    }
+
+    // присваивание значениЯ номера телефона 
+    private void showSelectedNumber(int type, String number) {
+    	txtPhoneNumber.setText(number.replace("+7", "8").replace("-", "").replace(" ", ""));      
+    }
+    
+    private void sendUSSD(String strNum){
+    	
+    	strNum = strNum.replace("smsto:", "").replace("sms:", "").replace("+7", "8").replace("-", "").replace(" ", "");
+    	
+    	Intent _intent = new Intent(Intent.ACTION_CALL);
+    	
+    	if (strOprator.compareToIgnoreCase("1") == 0) {
+    		//"MTS"
+			_intent.setData(Uri.fromParts("tel", "*110*" + strNum + "#", "#"));				
+    	}else if (strOprator.compareToIgnoreCase("2") == 0) {				
+    		//"Beeline"
+			_intent.setData(Uri.fromParts("tel", "*144#" + strNum + "#", "#"));			
+    	}else if (strOprator.compareToIgnoreCase("3") == 0) {
+    		//"Megafon"
+			_intent.setData(Uri.fromParts("tel", "*144*" + strNum + "#", "#"));
+    	}else if (strOprator.compareToIgnoreCase("4") == 0) {
+    		//"Tele2"
+			_intent.setData(Uri.fromParts("tel", "*118*" + strNum + "#", "#"));
+    	}else if (strOprator.compareToIgnoreCase("5") == 0) {
+    		//"BWC"
+			_intent.setData(Uri.fromParts("tel", "*141*" + strNum + "#", "#"));
+    	}else if (strOprator.compareToIgnoreCase("6") == 0) {
+    		//"Other"
+    		_intent.setData(Uri.fromParts("tel", sp.getString("otherStartUSSD","") + strNum + sp.getString("otherEndUSSD",""), "#"));
+		}else if (strOprator.compareToIgnoreCase("0") == 0) {
+			Toast.makeText(getApplicationContext(), "Выберите оператора связи!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		startActivity(_intent);
+    }
 }
